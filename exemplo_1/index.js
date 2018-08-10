c0b359d115a1c69ae97feb2966abf59fb8676746
Bráulio Figueiredo Pinto <braulio@braulioti.com.br>
2018-08-10 17:52:02 -0300

    sessão 4 aula 15

--- exemplo_1/index.js
+++ exemplo_1/index.js
@@ -1,27 +1,32 @@
-const ourHello = React.createElement('h1', null, 'Olá!');
+class OurHelloWithClass extends React.Component {
+    render() {
+        return <h1 className="my-title">Olá!</h1>
+    }
+}
 
-const ourHelloWithClass = React.createElement(
-    'h1',
-    { className: 'my-title' },
-    'Olá!'
-);
+class OurList extends React.Component {
+    render() {
+        return (
+            <ul>
+                <li className="item-01">Item 01</li>
+                <li className="item-02">Item 02</li>
+                <li className="item-03">Item 03</li>
+            </ul>
+        )
+    }
+}
 
-const ourList = React.createElement(
-    'ul',
-    null,
-    [
-        React.createElement('li', { className: 'item-01' }, 'Item 01'),
-        React.createElement('li', { className: 'item-02' }, 'Item 02'),
-        React.createElement('li', { className: 'item-03' }, 'Item 03')
-    ]
-);
-
-const myWrapper = React.createElement(
-    'div',
-    { className: 'my-wrapper' },
-    [ ourHelloWithClass, ourList ]
-);
+class MyWrapper extends React.Component {
+    render() {
+        return (
+            <div className="my-wrapper">
+                <OurHelloWithClass/>
+                <OurList/>
+            </div>
+        )
+    }
+}
 
 const container = document.getElementById('app');
 
-ReactDOM.render(myWrapper, container);
+ReactDOM.render(<MyWrapper/>, container);
