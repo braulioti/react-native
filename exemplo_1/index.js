c24a7b76be192213ef1a2780864aefd2ee40f54d
Bráulio Figueiredo Pinto <braulio@braulioti.com.br>
2018-08-10 15:17:25 -0300

    sessão 4 aula 14

--- exemplo_1/index.js
+++ exemplo_1/index.js
@@ -1,4 +1,27 @@
-const ourTitle = React.createElement('h1', null, 'Olá!');
+const ourHello = React.createElement('h1', null, 'Olá!');
+
+const ourHelloWithClass = React.createElement(
+    'h1',
+    { className: 'my-title' },
+    'Olá!'
+);
+
+const ourList = React.createElement(
+    'ul',
+    null,
+    [
+        React.createElement('li', { className: 'item-01' }, 'Item 01'),
+        React.createElement('li', { className: 'item-02' }, 'Item 02'),
+        React.createElement('li', { className: 'item-03' }, 'Item 03')
+    ]
+);
+
+const myWrapper = React.createElement(
+    'div',
+    { className: 'my-wrapper' },
+    [ ourHelloWithClass, ourList ]
+);
+
 const container = document.getElementById('app');
 
-ReactDOM.render(ourTitle, container);
+ReactDOM.render(myWrapper, container);
